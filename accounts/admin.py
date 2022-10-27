316ed00afa56dfa9f0e48df381c2fe715f2b4d7a
AliBkt <alibaktash740@yahoo.com>
2022-10-27 12:52:50 +0330

    add fieldsets in admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,8 @@ from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+
+
 # Register your models here.
 
 
@@ -10,7 +12,12 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
-
