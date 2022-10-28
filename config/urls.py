a3223e50d41937cf8054c7b895d1feed005543cc
AliBkt <alibaktash740@yahoo.com>
2022-10-28 13:02:30 +0330

    remove home path

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', TemplateView.as_view(template_name='home.html'), name="home"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
 ]
