6c3f09eab1c101b9d103974d22be9dd920a89ab4
AliBkt <alibaktash740@yahoo.com>
2022-10-28 15:11:11 +0330

    write test for home page

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+
+class HomeTests(TestCase):
+
+    def test_home_page_with_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_with_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_cantian(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Home Page')
