1b26e9c61dc1ce9d41ca69267d606fc24f3ad072
AliBkt <alibaktash740@yahoo.com>
2022-10-30 00:03:43 +0330

    Implimention rest password for user

--- config/settings.py
+++ config/settings.py
@@ -132,8 +132,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 # Authentication Setting
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Setting For Crispy Form
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
