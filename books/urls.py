47d27da234e39fff5f479f173d4bdf92130aa582
AliBkt <alibaktash740@yahoo.com>
2022-11-02 23:00:55 +0330

    add CreateBookeView

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name="book_list"),
-    path('<int:pk>/', views.BookDetailView.as_view(), name="book_deatail")
+    path('<int:pk>/', views.BookDetailView.as_view(), name="book_deatail"),
+    path('add/', views.BookCreateView.as_view(), name="add_book"),
 ]
 
--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 
 
 from .models import Book
+from .forms import BookForm
 
 
 class BookListView(generic.ListView):
@@ -15,4 +16,12 @@ class BookDetailView(generic.DetailView):
     model = Book
     template_name = "books/DeatailBook.html"
     context_object_name = 'book'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    # form_class is optional if fields be defined
+    # form_class = BookForm
+    fields = ['title', 'discription', 'author', 'price']
+    template_name = "books/add_book.html"
 
