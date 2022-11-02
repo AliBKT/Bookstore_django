f95732ddc7bf08ad2040d1d0db4acf43fd4a8ad9
AliBkt <alibaktash740@yahoo.com>
2022-11-02 19:11:58 +0330

    Setting booklist view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+from .models import Book
+
+
+class BookListView(generic.ListView):
+    template_name = "books/book_list.html"
+    model = Book
+    context_object_name = 'books'
