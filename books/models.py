8a29b79d3a74038a90b5272fb3e41b88996c1a17
AliBkt <alibaktash740@yahoo.com>
2022-11-06 23:57:05 +0330

    Add image to book and display in site

--- books/models.py
+++ books/models.py
@@ -7,6 +7,7 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     discription = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
 
     def __str__(self):
         return f'{self.author} : {self.title}'
