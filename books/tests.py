bf1257f14d6afb2a72e2d8d0c4530ab266923301
Ali Baktash <alibaktash740@yahoo.com>
2022-11-19 11:50:24 -0500

    write others test for books app.

--- books/tests.py
+++ books/tests.py
@@ -17,27 +17,97 @@ class BookTests(TestCase):
             'example@gmail.com',
             "Asder\9874",
         )
-        self.user = self.model.objects.last()
-        Book.objects.create(title='test title', discription='test discription', author='test author', price=15,
-                            user=self.user, cover='book/img.png')
-        self.book = Book.objects.last()
+        self.user = self.model.objects.first()
 
-    def test_detail_view(self):
+        self.model = get_user_model()
+        self.model.objects.create_user(
+            'UserTest2',
+            'example2@gmail.com',
+            "Asder\98747",
+        )
+        self.user2 = self.model.objects.last()
+
+        Book.objects.create(title='test title', discription='test discription', author='test author', price=15,
+                            user=self.user2, cover='')
+        self.book_without_cover = Book.objects.first()
+
+        Book.objects.create(title='test title', discription='test discription', author='test author', price=15,
+                            user=self.user, cover='media/test.png')
+        self.book_with_cover = Book.objects.last()
+
+    def test_detail_view_without_cover(self):
         self.client.force_login(self.user)
-        response = self.client.get(reverse('book_deatail', args=[self.book.id]))
+        response = self.client.get(reverse('book_deatail', args=[self.book_without_cover.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_view_with_cover(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('book_deatail', args=[self.book_with_cover.id]))
         self.assertEqual(response.status_code, 200)
 
     def test_add_book_view_create_book(self):
         response = self.client.post(reverse('add_book'), {
-             'title': 'test title',
-             'discription': 'test discription',
-             'author': 'test author',
-             'price': 15,
-             'user': self.user
-         })
+            'title': 'test title',
+            'discription': 'test discription',
+            'author': 'test author',
+            'price': 15,
+            'user': self.user
+        })
         self.assertEqual(response.status_code, 302)
 
     def test_add_book_view_template(self):
         self.client.force_login(self.user)
         response = self.client.get(reverse('add_book'))
         self.assertEqual(response.status_code, 200)
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_book_list_view_contain_book(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertContains(response, 'test discription')
+
+    def test_book_list_url(self):
+        response = self.client.get('/books/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_book_view_name(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('delete_book', args=[self.book_with_cover.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_book_view_can_delete_book(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('delete_book', args=[self.book_with_cover.id]))
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_book_view_can_delete_book_with_other_user(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('delete_book', args=[self.book_without_cover.id]))
+        self.assertEqual(response.status_code, 403)
+
+    def test_update_book_view_can_update(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('update_book', args=[self.book_with_cover.id]), {
+            'title': 'test title',
+            'discription': 'test discription1',
+            'author': 'test author',
+            'price': 15,
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_update_book_view(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('update_book', args=[self.book_with_cover.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_book_view_can_update_with_other_user(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('update_book', args=[self.book_without_cover.id]), {
+            'title': 'test title',
+            'discription': 'test discription4',
+            'author': 'test author',
+            'price': 15,
+        })
+        self.assertEqual(response.status_code, 403)
